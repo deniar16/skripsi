47e9a1d816ea2ba09544d86afbfcf88792eedc8b
deniarianto1606 <deni.arianto1606@gmail.com>
2017-09-21 11:38:16 +0700

    implement findByLatitudeAndLongitude with native query

--- maps/maps-server/src/main/java/com/mercubuana/skripsi/repositories/MapRepository.java
+++ maps/maps-server/src/main/java/com/mercubuana/skripsi/repositories/MapRepository.java
@@ -9,4 +9,7 @@ public interface MapRepository extends JpaRepository<Map, Integer> {
 
     @Query("SELECT m FROM Map m where m.latitude < :latitude and m.longitude >= :longitude")
     public Map findByLatitudeAndLongitude(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
+
+    @Query(value = "SELECT * FROM Map m where m.latitude < :latitude and m.longitude >= :longitude LIMIT 1", nativeQuery = true)
+    public Map findByLatitudeAndLongitudeNative(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
 }
